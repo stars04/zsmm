8cab405b8182f0e739ba0eaeeee1652fd098f1bd
stars04 <starwas1234@yahoo.com>
2025-09-14 10:18:16 -0400

    first commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use iced::{Background, Border, Color, Element, Length, Renderer, Task};
+use iced::alignment::{Horizontal,Vertical};
+use iced::event::{self,Status};
+use iced::widget::{button, column, container, row, text, text_input};
+use iced_core::{Shadow, Theme, border::Radius};
+use std::env::home_dir;
+use std::fs;
+use std::path::PathBuf;
+
+
+#[tokio::main]
+async fn main() {
+    println!("Hello, world!");
+}
