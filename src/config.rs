8381185c26c30de636495bfa11db53a93f3e51ac
stars04 <starwas1234@yahoo.com>
2025-10-29 19:20:14 -0400

    loading configs functional, edge case discovered with locating pngs, needs rework

--- src/config.rs
+++ src/config.rs
@@ -53,32 +53,41 @@ pub async fn load_workshop_location() -> Option<String> {
 
 pub async fn save_workshop_location(mods_directory: String) {
     let mut output: String = String::new();
+
     output.push_str(&mods_directory);
     
     let _ = fs::write(LIN_CONFIG_LOC.to_owned() + "workshop_location", output).await;
 }
-pub async fn write_config(
+pub async fn write_selection_config(
     file_name: String,
     selections: HashMap<String, bool>,
+    mod_ids: Vec<String>,
 ) {
     let mut output: String = String::new();
+
     let config_file = LIN_CONFIG_LOC.to_owned() + &file_name;
 
-
-    for (id, bool) in selections {
-        let insertion = format!("<{},{}>", id, bool);
-
-        output.push_str(&insertion);
+    for (name, bool) in selections {
+        output.push_str(&format!("{},{};", name, bool));
     }
+
+    output.push('\n');
+
+    for id in mod_ids {
+        output.push_str(&format!("{};",&id));
+    } 
+    
 
     let _ = fs::write(config_file, output).await;
 }
 
 //TODO: CLEAN THIS TF UP
-pub async fn read_config(file_name: &str) -> HashMap<String, bool> {
+pub async fn read_config(file_name: String) -> (Vec<String>, HashMap<String, bool>) {
     let mut output_map: HashMap<String, bool> = HashMap::new();
+    let mut mod_id_vec: Vec<String> = Vec::new();
     let mut buffer: Vec<u8> = Vec::new();
-    let config_path: String = LIN_CONFIG_LOC.to_owned() + file_name;
+
+    let config_path: String = file_name;
     let mut file = match File::open(&config_path).await {
         Ok(file) => file,
         Err(err) => panic!("Error reading {} -> Err: {}", config_path, err),
@@ -87,41 +96,40 @@ pub async fn read_config(file_name: &str) -> HashMap<String, bool> {
 
     if let Ok(text) = str::from_utf8(&buffer) {
         let mut string: String = text.to_string();
+        println!("{:?}", string);
         let mut inspection: String;
         let mut chop: usize;
         let mut comma_loc: usize;
         let mut key: String;
         let mut value: String;
 
-        //let initial_offset = string.find(':').unwrap() + 2_usize;
-        //string = string.split_off(initial_offset);
+        //string = string.replace("\n", "");
+        while !string.is_empty() {
+            let ret_location: Option<usize> = string.find("\n");
+            inspection = string.clone();
+            chop = inspection.find(';').unwrap();
+            inspection.replace_range(chop.., "");
+            string.replace_range(..(chop + 1), "");
 
-        //let new_line = string.find('\n').unwrap();
-
-        //output_string = string.to_string().clone();
-        //output_string.replace_range(new_line.., "");
-
-        //string.replace_range(..(new_line + 1_usize), "");
-        string = string.replace("\n", "");
-        loop {
-            if !string.is_empty() {
-                inspection = string.clone();
-                chop = inspection.find('>').unwrap();
-                inspection.replace_range(chop.., "");
-                string.replace_range(..(chop + 1), "");
-
+            if ret_location.is_some() && ret_location != Some(0_usize) {
                 comma_loc = inspection.find(',').unwrap();
                 value = inspection.split_off(comma_loc).replace(",", "");
-                key = inspection.replace("<", "");
+                key = inspection;
 
                 output_map.insert(key, value.parse::<bool>().unwrap());
-            } else {
-                break;
+            }else if ret_location == Some(0) {
+                inspection = inspection.replace("\n", "");
+                string.replace_range(..ret_location.unwrap(), "");
+
+                mod_id_vec.push(inspection);
+            }else if ret_location.is_none() {
+                mod_id_vec.push(inspection);
             }
         }
+        
     }
 
-    output_map
+    (mod_id_vec, output_map)
 }
 
 #[cfg(test)]
@@ -130,7 +138,9 @@ mod tests {
 
     #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
     async fn testing() {
-        let result = save_workshop_location("/mnt/d1/SSD1/steamapps/workshop/content/108600/".to_string()).await;
-        //let result = write_config("initial".to_string(), "/mnt/d1/SSD1/steamapps/workshop/content/108600/".to_string(),HashMap::new()).await;
+        let result = read_config("/home/star/.config/zsmm/test".to_string()).await;
+        println!("\n\n{:?}", result);
+        //let result = write_selection_config("initial".to_string(), "/mnt/d1/SSD1/steamapps/workshop/content/108600/".to_string(),HashMap::new()).await;
+                
     }
 }
